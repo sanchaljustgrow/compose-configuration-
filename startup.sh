90f3c1f59efaa2426729188a65eb53f4b7a72525
sanchaljustgrow <sanchal.khedkar@justgrowinfotech.com>
2025-11-06 12:55:23 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 echo "Hello, world!"
 echo "My first script is running."
+echo "Stopping existing containers..."
+docker compose down
+
 
 # You can take input from arguments passed to the script
 echo "The first argument passed was: $1"
 
 ENV=$1
 echo "Really want to pass ENV =$1 "
-
-
-docker-compose up -d
-
-echo "Stopping existing containers..."
-docker compose down
+if ENV == YES 
 
 echo "Starting containers with new configuration..."
-docker compose up -d
+docker-compose up -d 
+
+
