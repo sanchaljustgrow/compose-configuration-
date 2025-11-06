db84b58ca82862e9c4699bb7181af5b775012b2f
sanchaljustgrow <sanchal.khedkar@justgrowinfotech.com>
2025-11-06 12:47:40 +0530

    Update startup.sh (#10)

--- startup.sh
+++ startup.sh
@@ -6,4 +6,13 @@ echo "My first script is running."
 echo "The first argument passed was: $1"
 
 ENV=$1
+echo "Really want to pass ENV =$1 "
+
+
 docker-compose up -d
+
+echo "Stopping existing containers..."
+docker compose down
+
+echo "Starting containers with new configuration..."
+docker compose up -d
